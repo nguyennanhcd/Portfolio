2bc1eb1737d5821826ddf9263dcfe5447eaa8e53
nguyenanhcd <anh487303@gmail.com>
2025-06-07 09:43:04 +0700

    feat: add routing and page transition effects

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css'
 
 //components
 import Header from '@/components/Header'
+import PageTransition from '@/components/PageTransition'
+import StairTransition from '@/components/StairTransition'
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ['latin'],
@@ -25,7 +27,8 @@ export default function RootLayout({
     <html lang='en' suppressHydrationWarning>
       <body className={jetbrainsMono.variable}>
         <Header />
-        {children}
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   )
---
+++ components/Stairs.tsx
@@ -0,0 +1,52 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+
+// variants
+const stairAnimation = {
+  initial: {
+    top: '0%'
+  },
+  animate: {
+    top: '100%'
+  },
+  exit: {
+    top: ['100%', '0%']
+  }
+}
+
+//calculate the reversed index for staggered animation
+const reversedIndex = (index: number) => {
+  const totalSteps = 6
+  return totalSteps - index - 1
+}
+
+const Stairs = () => {
+  return (
+    <>
+      {/* render a 6 motion divs, each representing a step of the stairs.
+       Each div will have the same animation defined by the stairAnimation object.
+        The delay for each div is calculated dynamically based on it's reversed index,
+         creating a staggered effect with decreasing delay for each subsequent step*/}
+
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial='initial'
+            animate='animate'
+            exit='exit'
+            transition={{
+              duration: 0.4,
+              ease: 'easeInOut',
+              delay: reversedIndex(index) * 0.1
+            }}
+            className='h-full w-full bg-white relative'
+          />
+        )
+      })}
+    </>
+  )
+}
+
+export default Stairs
