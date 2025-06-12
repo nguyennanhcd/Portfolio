69637e3485dfcf406add25d23cddab52b678066e
nguyenanhcd <anh487303@gmail.com>
2025-06-12 18:49:42 +0700

    feat: done social and download cv button

---
+++ constants/socials.tsx
@@ -0,0 +1,8 @@
+import { FaGithub, FaFacebook, FaLinkedin, FaYoutube } from 'react-icons/fa'
+
+export const socials = [
+  { icon: <FaGithub />, path: '' },
+  { icon: <FaFacebook />, path: '' },
+  { icon: <FaLinkedin />, path: '' },
+  { icon: <FaYoutube />, path: '' }
+]
