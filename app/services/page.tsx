abc31b80768b2e116fcbab7c8b8d2261616db663
nguyenanhcd <anh487303@gmail.com>
2025-06-13 09:39:27 +0700

    feat: create Services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,58 @@
+'use client'
+import { BsArrowDownRight } from 'react-icons/bs'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+
+//constants
+import { services } from '@/constants/services'
+
 const Services = () => {
-  return <div>Services page</div>
+  return (
+    <section className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0 ml-2'>
+      <div className='container mx-auto'>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 1, duration: 0.4, ease: 'easeInOut' },
+          }}
+          className='grid grid-cols-1 md:grid-cols-2 gap-[60px]'
+        >
+          {services.map((service, index) => {
+            return (
+              <div
+                key={index}
+                className='flex-1 flex flex-col justify-center gap-6 group'
+              >
+                {/* top */}
+                <div className='w-full flex items-center justify-between'>
+                  <div className='text-5xl font-extrabold text-outline text-transparent text-outline-hover transition-all duration-500'>
+                    {service.num}
+                  </div>
+                  <Link
+                    href={service.href}
+                    className='w-[60px] h-[60px] rounded-full bg-white group-hover:bg-accent-default transition-all duration-500 flex justify-center items-center hover:-rotate-45'
+                  >
+                    <BsArrowDownRight className='text-primary text-2xl' />
+                  </Link>
+                </div>
+                {/* title*/}
+                <h2 className='text-[42px] font-bold leading-none text-white  group-hover:text-accent-default transition-all duration-500'>
+                  {service.title}
+                </h2>
+                {/* description*/}
+                <p className='text-white/60'>{service.description}</p>
+                {/* border*/}
+                <div className='border-b border-white/20 w-full'>
+                  {service.title}
+                </div>
+              </div>
+            )
+          })}
+        </motion.div>
+      </div>
+    </section>
+  )
 }
 
 export default Services
---
+++ constants/services.tsx
@@ -0,0 +1,26 @@
+export const services = [
+  {
+    num: '01',
+    title: 'Web Developer',
+    description: 'lorem ipsum',
+    href: '',
+  },
+  {
+    num: '02',
+    title: 'UI/UX Designer',
+    description: 'lorem ipsum',
+    href: '',
+  },
+  {
+    num: '03',
+    title: 'Content Creator',
+    description: 'lorem ipsum',
+    href: '',
+  },
+  {
+    num: '04',
+    title: 'SEO',
+    description: 'lorem ipsum',
+    href: '',
+  },
+]
