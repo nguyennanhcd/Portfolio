4a4106cdbbd6761cc4879352cb349d92f8a9812d
nguyenanhcd <anh487303@gmail.com>
2025-06-14 08:44:31 +0700

    feat: build fundamental things for work page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,5 +1,28 @@
+'use client'
+
+import { projects } from '@/constants/projects'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+
 const Work = () => {
-  return <div>Work page</div>
+  const [project, setProject] = useState(projects[0])
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'
+    >
+      <div className='container mx-auto'>
+        <div className='flex flex-col xl:flex-row xl:gap-[30px]'>
+          <div className='w-full xl:w-[50%] xl:h-[460px] flex flex-col xl: justify'>
+            text
+          </div>
+          <div className='w-full xl:w-[50%]'>slider</div>
+        </div>
+      </div>
+    </motion.section>
+  )
 }
 
 export default Work
---
+++ constants/projects.tsx
@@ -0,0 +1,36 @@
+export const projects = [
+  {
+    num: '01',
+    category: 'frontend',
+    title: 'project 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, dicta alias iusto voluptatum at repellendus assumenda, expedita facere cum sint hic, odit quidem facilis sequi deleniti eaque unde aspernatur error.',
+    stack: [{ name: 'Html5' }, { name: 'Css3' }, { name: 'Javascript' }],
+    image: '/assets/work/thumb1.png',
+    live: '',
+    github: '',
+  },
+
+  {
+    num: '01',
+    category: 'fullstack',
+    title: 'project 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, dicta alias iusto voluptatum at repellendus assumenda, expedita facere cum sint hic, odit quidem facilis sequi deleniti eaque unde aspernatur error.',
+    stack: [{ name: 'Html5' }, { name: 'Css3' }, { name: 'Javascript' }],
+    image: '/assets/work/thumb1.png',
+    live: '',
+    github: '',
+  },
+  {
+    num: '01',
+    category: 'frontend',
+    title: 'project 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, dicta alias iusto voluptatum at repellendus assumenda, expedita facere cum sint hic, odit quidem facilis sequi deleniti eaque unde aspernatur error.',
+    stack: [{ name: 'Html5' }, { name: 'Css3' }, { name: 'Javascript' }],
+    image: '/assets/work/thumb1.png',
+    live: '',
+    github: '',
+  },
+]
