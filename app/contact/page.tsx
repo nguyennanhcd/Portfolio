62f053caf0029055129dae6dab2733130d0c5347
nguyenanhcd <anh487303@gmail.com>
2025-06-17 15:15:05 +0700

    chore: change project directory

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,17 +1,7 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
-import { Textarea } from '@/components/ui/textarea'
+import Form from '@/components/Form'
+
 import { info } from '@/constants/info'
 import { motion } from 'framer-motion'
 
@@ -32,73 +22,7 @@ const Contact = () => {
       <div className='container mx-auto'>
         <div className='flex flex-col xl:flex-row gap-[30px]'>
           {/* form */}
-          <div className='xl:w-[70%] order-2 xl:order-none'>
-            <form
-              action=''
-              className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl'
-            >
-              <h3 className='text-4xl text-accent-default'>
-                Let&rsquo;s work together
-              </h3>
-              <p className='text-white/60'>
-                Ready to bring your vision to life? Whether
-                you need a stunning website, personalized
-                tutoring, English mastery, or top-tier SEO,
-                I’m here to deliver exceptional results.
-                With expertise in crafting dynamic digital
-                solutions and tailored learning experiences,
-                I partner with you to achieve your goals.
-                Let’s connect and create something
-                extraordinary!
-              </p>
-              <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-                <Input
-                  type='firstName'
-                  placeholder='First name'
-                />
-                <Input
-                  type='lastName'
-                  placeholder='Last name'
-                />
-                <Input type='email' placeholder='Email' />
-                <Input type='phone' placeholder='Phone' />
-              </div>
-              {/* select */}
-              <Select>
-                <SelectTrigger className='w-full'>
-                  <SelectValue placeholder='Select a service' />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>
-                      Select a service
-                    </SelectLabel>
-                    <SelectItem value='est'>
-                      Web Development
-                    </SelectItem>
-                    <SelectItem value='cst'>
-                      SEO{' '}
-                    </SelectItem>
-                    <SelectItem value='mst'>
-                      MMO Tools
-                    </SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              {/*text area */}
-              <Textarea
-                className='h-[200px]'
-                placeholder='Type your message here...'
-              />
-              {/*button */}
-              <Button
-                className='max-w-40 cursor-pointer'
-                size='md'
-              >
-                Send message
-              </Button>
-            </form>
-          </div>
+          <Form />
 
           {/* info */}
           <div className='flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0 xl:w-[40%]'>
