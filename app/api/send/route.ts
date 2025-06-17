5958988d403eb06fbc95552bd47033a61c8e89a7
nguyenanhcd <anh487303@gmail.com>
2025-06-17 22:09:44 +0700

    feat: a little bit change

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
     const resend = new Resend(process.env.RESEND_API_KEY)
     try {
         const {data} = await resend.emails.send({
-            from: 'anhportfolio.vercel.app',
+            from: 'onboarding@resend.dev',
             to: 'anh487303@gmail.com',
             subject: 'Hello World',
             html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
