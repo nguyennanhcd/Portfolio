48a51f791e092e543f4610936205191910d0c5b5
nguyenanhcd <anh487303@gmail.com>
2025-06-24 17:01:45 +0700

    perf: add fetchPriority='high'

---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
