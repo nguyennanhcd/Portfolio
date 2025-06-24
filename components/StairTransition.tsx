0bc216175dea41a9650eff6c4dbdc658d8573db4
nguyenanhcd <anh487303@gmail.com>
2025-06-24 18:55:31 +0700

    perf: improve cls

--- components/StairTransition.tsx
+++ components/StairTransition.tsx
@@ -24,7 +24,6 @@ const StairTransition = () => {
           <div className='h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex'>
             <Stairs />
           </div>
-
           <MotionDiv
             className='h-screen w-screen fixed bg-primary top-0 pointer-events-none'
             initial={{ opacity: 1 }}
