4396cdc21f228dfbac6c508c9fa96f6865a5e56a
nguyenanhcd <anh487303@gmail.com>
2025-07-03 20:10:50 +0700

    done header Animation

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,23 +1,31 @@
 'use client'
+
 import { links } from '@/constants/navLinks'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-const Nav = () => {
-  const pathname: string = usePathname()
+type NavProps = { scrolled: boolean }
+
+const Nav = ({ scrolled }: NavProps) => {
+  const pathname = usePathname()
+
   return (
     <nav className='flex gap-8'>
-      {links.map((link, index) => {
+      {links.map((link) => {
+        const isActive = link.path === pathname
+
+        const activeClasses = isActive
+          ? scrolled
+            ? 'text-primary border-b-2 border-primary hover:text-primary/80'
+            : 'text-accent-default border-b-2 border-accent-default hover:text-accent-hover'
+          : ''
+
         return (
           <Link
-            key={index}
+            key={link.path}
             href={link.path}
-            className={`${
-              link.path === pathname &&
-              'text-accent-default border-b-2 border-accent-default'
-            } capitalize font-medium hover:text-accent-hover transition-all`}
+            className={`capitalize font-medium transition-all ${activeClasses} ${scrolled ? 'text-primary/80 hover:text-primary hover:text-base font-bold transition-all' : 'text-white hover:text-accent-hover'}`}
           >
-            {' '}
             {link.name}
           </Link>
         )
